17ae7d8a71c7759e053cb7d003b8934db9105e28
Hari Charan <haricharanbonam@gmail.com>
2026-04-10 23:07:33 +0530

    nithg

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,11 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: "https://haricharanbonam.tech/blog",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
   ];
 }
